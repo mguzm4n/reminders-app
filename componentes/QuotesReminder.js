829309cec44b82169bf39caee95ebd39433db2aa
Marcelo <marcelo.guzman.g@usach.cl>
2022-09-18 03:16:41 -0300

    style quoting

--- componentes/QuotesReminder.js
+++ componentes/QuotesReminder.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { useEffect, useState } from 'react';
 import { getDataFrom, addItemTo } from '../utils';
 
@@ -36,18 +36,56 @@ const QuotesReminder = () => {
   }, []);
 
   return(
-    <View style={{alignItems: 'center', width: '100%'}}>
-      <Text style={{textAlign: 'left'}}>Nombre del autor</Text>
-      <TextInput onChangeText={txt => onInputTextChange('author', txt)} placeholder="Movie-name" />
+    <View style={{marginTop: 10, alignItems: 'center', width: '100%'}}>
+      <View style={ styles.form }>
+        <Text style={ styles.title }>Crea una nueva cita</Text>
 
-      <Text style={{textAlign: 'left'}}>Texto de la cita</Text>
-      <TextInput onChangeText={txt => onInputTextChange('quoteContent', txt)} placeholder="Movie-img" />
+        <Text>Nombre del autor</Text>
+        <TextInput style={ styles.textInput }
+          onChangeText={txt => onInputTextChange('author', txt)} placeholder="Quote author" />
+        
+        <Text style={{textAlign: 'left'}}>Texto de la cita</Text>
+        <TextInput multiline={true} style={styles.textareaInput}
+          onChangeText={txt => onInputTextChange('quoteContent', txt)} placeholder="Quote text" />
+        <Button style={ styles.submitBtn }
+          onPress={addNewItem} title="Añadir cita" />
+      </View>
 
-      <Button onPress={addNewItem} title="Añadir cita" />
-
-      {quotes.map((q) => <View><Text>{ q.author }</Text></View>)}
+      {quotes.map(({ author, quoteContent }) => {
+        return(
+          <View>
+            <Text>{ author }</Text>
+            <Text>{ quoteContent }</Text>
+          </View>
+        )}
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  form:{
+    width: '85%',
+    justifyContent: 'flex-start',
+  },
+  textInput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  textareaInput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginBottom: 10,
+  },
+  submitBtn: {
+    margin: 15,
+  },
+});
 
 export default QuotesReminder;
